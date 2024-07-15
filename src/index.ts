fd07af1aa8da96dc6f5aeb71c6086db694a35eb2
Ronald A. Richardson <me@ron.dev>
2024-07-15 17:01:43 +0800

    increased tar package url expiration to 30 minutes for slower builds or slow connections

--- src/index.ts
+++ src/index.ts
@@ -420,7 +420,7 @@ export default class S3Database implements IPluginStorage<S3Config> {
 
         const fileName = `${nameWithoutScope}-${version}.tgz`;
         const tarballPath = this._getPackagePath(packageName, fileName);
-        const signedUrlExpireSeconds = 60 * 5;
+        const signedUrlExpireSeconds = 60 * 30;
 
         let url = this.s3.getSignedUrl('getObject', {
             Bucket: this.config.bucket,
